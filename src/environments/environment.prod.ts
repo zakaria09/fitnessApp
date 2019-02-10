ae668178961bfd452bd30554e6086d4047956ec4
zakaria09 <zakaria.09@hotmail.co.uk>
2019-02-10 20:42:06 +0000

    a few fixes as well as early deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA5EcdqP9nkhTpkJQVZUuCJC8NKxWQtNgw",
+    authDomain: "fitness-tracker-3e4ed.firebaseapp.com",
+    databaseURL: "https://fitness-tracker-3e4ed.firebaseio.com",
+    projectId: "fitness-tracker-3e4ed",
+    storageBucket: "fitness-tracker-3e4ed.appspot.com",
+    messagingSenderId: "697829454488"
+  }
 };
